b929385834820ea5d0d06ade512ef77a4403fe1d
Jorge Chable <yahicimosclick9193@gmail.com>
2020-09-19 01:19:03 -0500

    Register view

---
+++ src/app/register/register.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register.component';
+import { RegisterRoutingModule } from './register-routing.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [CommonModule, RegisterRoutingModule, FormsModule],
+})
+export class RegisterModule {}
