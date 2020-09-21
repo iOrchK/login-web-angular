408f6fd59e9a5de62033126ab7498516442e1080
Jorge Chable <yahicimosclick9193@gmail.com>
2020-09-21 12:27:21 -0500

    Cambios en Login Module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, FormsModule],
+  imports: [CommonModule, LoginRoutingModule, FormsModule, ReactiveFormsModule],
 })
 export class LoginModule {}
