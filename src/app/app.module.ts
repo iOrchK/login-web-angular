296a16d4070419b495ab1757ca43b107db2a3475
Jorge Chable <yahicimosclick9193@gmail.com>
2020-09-21 12:25:36 -0500

    App Module - importación de interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationService } from './login/shared/authentication.service';
 import { CoreModule } from './core/core.module';
+import { InterceptorService } from './core/services/interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +18,10 @@ import { CoreModule } from './core/core.module';
     HttpClientModule,
     CoreModule,
   ],
-  providers: [AuthenticationService],
+  providers: [
+    AuthenticationService,
+    { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,18 +2,11 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { StorageService } from './services/storage.service';
 import { AuthorizatedGuard } from './guards/authorizated.guard';
 import { SecurityDataService } from './services/security-data.service';
-import { InterceptorService } from './services/interceptor.service';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [],
-  providers: [
-    StorageService,
-    AuthorizatedGuard,
-    SecurityDataService,
-    { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true },
-  ],
+  providers: [StorageService, AuthorizatedGuard, SecurityDataService],
   bootstrap: [],
 })
 export class CoreModule {
