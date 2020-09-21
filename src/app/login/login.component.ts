d49c24bc8ac9781f57b216a90dca78806d446b34
Jorge Chable <yahicimosclick9193@gmail.com>
2020-09-21 09:55:27 -0500

    Login - Iniciar sesión

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,36 +1,89 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { LoginObject } from './shared/login-object.model';
+import { AuthenticationService } from './shared/authentication.service';
+import { StorageService } from '../core/services/storage.service';
+import { Session } from '../core/models/session.model';
+import { SecurityDataService } from '../core/services/security-data.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  providers: [AuthenticationService],
 })
 export class LoginComponent implements OnInit {
-  public email: string;
-  public password: string;
+  public login: LoginObject;
+  private validations: any;
+  public errorEmail: string;
+  public errorPassword: string;
+  public statusSuccess: number[];
+  public isLoading: boolean;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private storageService: StorageService,
+    private authService: AuthenticationService,
+    private securityDataService: SecurityDataService
+  ) {}
 
   ngOnInit(): void {
-    this.email = '';
-    this.password = '';
+    this.login = new LoginObject();
+    this.validations = {
+      REQUIRED: 'Campo requerido',
+    };
+    this.errorEmail = this.validations.REQUIRED;
+    this.errorPassword = this.validations.REQUIRED;
+    this.statusSuccess = [200, 201];
+    this.isLoading = false;
   }
 
   handleEmail() {
-    console.log(this.email);
+    this.errorEmail = !this.login.email ? this.validations.REQUIRED : null;
   }
 
   handlePassword() {
-    console.log(this.password);
+    this.errorPassword = !this.login.password
+      ? this.validations.REQUIRED
+      : null;
+  }
+
+  validateForm() {
+    return this.errorEmail || this.errorPassword;
   }
 
   onLogIn() {
-    if (this.email && this.password) {
-      this.ngOnInit();
-      this.router.navigate(['home']);
+    if (!this.login.email || !this.login.password) {
       return;
     }
-    alert('Please check the fields');
+    this.isLoading = true;
+    const newLogin = new LoginObject(this.login);
+    const ciphertext = this.securityDataService.CipherText(newLogin.password);
+    console.log(ciphertext);
+    newLogin.password = ciphertext;
+    this.authService.logIn(newLogin).subscribe(
+      (response) => {
+        this.setSession(new Session(response));
+      },
+      (error) => {
+        this.isLoading = false;
+        let { message } = error.error;
+        if (!message) {
+          message = 'La conexión a la fuente de datos ha fallado';
+        }
+        Swal.fire({
+          title: 'Error!',
+          text: message,
+          icon: 'error',
+          confirmButtonText: 'Cerrar',
+        });
+      }
+    );
+  }
+
+  private setSession(data: Session) {
+    this.storageService.setCurrentSession(data);
+    this.router.navigate(['home']);
   }
 }
